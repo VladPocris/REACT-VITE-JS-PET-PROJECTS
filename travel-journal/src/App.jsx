5a09043ec58ce4d3e62ff9cf048e21a50e6634b4
Vlad Pocris <vpocris@gmail.com>
2026-04-17 06:11:41 +0300

    Final iteration Travel Journal

--- travel-journal/src/App.jsx
+++ travel-journal/src/App.jsx
@@ -1,11 +1,16 @@
 import Header from "./components/Header.jsx"
 import Entry from "./components/Entry.jsx"
+import Data from "../src/data/data.js"
 
 export default function App() {
+  const entryElements = Data.map(({id, ...rest}) => <Entry 
+        key = {id}
+        {...rest}
+      />)
   return(
     <>
       <Header />
-      <Entry />
+      {entryElements}
     </>
   )
 }
--- travel-journal/src/components/Entry.jsx
+++ travel-journal/src/components/Entry.jsx
@@ -1,19 +1,19 @@
-export default function Entry() {
+export default function Entry(props) {
     return (
         <article className="entry">
             <div className="entry-container">
-                <img className="entry-container-image" src="./src/assets/mount-fuji.jpg" alt="Picture of mount Fuji"></img>
+                <img className="entry-container-image" src={props.img.src} alt={props.img.alt}></img>
             </div>
             <div className="entry-content">
                 <div className="entry-content-reference">
                     <img className="entry-content-reference-marker" src="./src/assets/marker.png" alt="Location marker"></img>
-                    <span className="entry-content-reference-location">Japan</span>
-                    <a className="entry-content-reference-link" href="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu">View on Google Maps</a>
+                    <span className="entry-content-reference-location">{props.country}</span>
+                    <a className="entry-content-reference-link" href={props.googleMapsLink}>View on Google Maps</a>
                 </div>
-                <h1 className="entry-content-title">Mount Fuji</h1>
-                <p className="entry-content-date">12 Jan, 2021 - 24 Jan, 2021</p>
+                <h1 className="entry-content-title">{props.title}</h1>
+                <p className="entry-content-date">{props.dates}</p>
                 <p className="entry-content-description">
-                        Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.
+                        {props.text}
                 </p>
             </div>
          </article>
